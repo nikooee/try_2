7deca82a28967a84b1d014a23a8d18b4cf82cae5
nikooee <nikooee.hamed@gmail.com>
2019-07-05 18:23:17 +0430

    15-4-98-Try-2, sakhtan avaliye data base dar model anjam shod va dar admin register shod

--- home_page/admin.py
+++ home_page/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import(Artist, Album, Image, Tag)
+
+
+admin.site.register(Artist)
+admin.site.register(Album)
+admin.site.register(Image)
+admin.site.register(Tag)
+
+
--- home_page/models.py
+++ home_page/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+
 # Create your models here.
+class Artist(models.Model):
+    f_name = models.CharField(max_length=20, null=True)
+    l_name = models.CharField(max_length=20, null=True)
+    nick_name = models.CharField(max_length=20, null=True)
+    avatar = models.ImageField(null=True, blank=False, upload_to='albums/%Y-%m-%d/')
+
+
+class Album(models.Model):
+    title = models.CharField(max_length=20, null=True)
+    description = models.CharField(max_length=20, null=True)
+    summary = models.TextField(null=True)
+    rate = models.FloatField(null=True, default=0)
+    like = models.PositiveIntegerField(null=True, default=0)
+    release_date = models.DateField(null=True)
+    cover = models.ImageField(null=True, blank=False, upload_to='albums/%Y-%m-%d/')
+
+
+class Image(models.Model):
+    title = models.CharField(max_length=20, null=True)
+    Image = models.FileField(null=True, blank=False, upload_to='albums/%Y-%m-%d/')
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=20, null=True)
